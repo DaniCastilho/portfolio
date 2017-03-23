b68539e60da5159c5a0c44471fc2ba4d0aadc99e
Dani Castilho <danicastilhodev@gmail.com>
2017-03-23 00:55:12 -0300

    iniciando dev

---
+++ src/app/app-header/app-header.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './app-header.component.html',
+  styleUrls: ['./app-header.component.css']
+})
+export class AppHeaderComponent implements OnInit {
+
+  text : string ='{{ Dani Castilho - Web Developer }}'
+  el : any = document.getElementsByClassName('app-header__title');
+
+  char : any ='';
+  writer : any ='';
+  next : any ='';
+  constructor() { }
+
+  typeWriter(text, el){
+    this.char = text.split('').reverse();
+    this.writer = setInterval(() => {
+      if (!this.char.length) return clearInterval(this.writer);
+      this.next = this.char.pop();
+      el.innerHTML += this.next;
+      // console.log(el.innerHTML += this.next)
+    }, 100)
+  }
+   
+
+  ngOnInit() {
+    
+    
+  }
+
+}
